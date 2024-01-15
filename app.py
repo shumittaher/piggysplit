7d8b9705e9a93ad2f96edeb49e372912a797ef2f
Shumit Taher <shumitt@gmail.com>
2024-01-15 13:15:35 +0600

    changes to helpers

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import re
 
 
-from helpers import login_required
+from helpers import login_required, password_check
 
 # Configure application
 app = Flask(__name__)
@@ -64,44 +64,3 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():   
     return render_template("login.html")
-
-def password_check(password):
-    """
-    Reference: https://stackoverflow.com/questions/16709638/checking-the-strength-of-a-password-how-to-check-conditions
-
-    Verify the strength of 'password'
-    Returns a dict indicating the wrong criteria
-    A password is considered strong if:
-        8 characters length or more
-        1 digit or more
-        1 symbol or more
-        1 uppercase letter or more
-        1 lowercase letter or more
-    """
-
-    # calculating the length
-    length_error = len(password) < 8
-
-    # searching for digits
-    digit_error = re.search(r"\d", password) is None
-
-    # searching for uppercase
-    uppercase_error = re.search(r"[A-Z]", password) is None
-
-    # searching for lowercase
-    lowercase_error = re.search(r"[a-z]", password) is None
-
-    # searching for symbols
-    symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
-
-    # overall result
-    password_ok = not (length_error or digit_error or uppercase_error or lowercase_error or symbol_error)
-
-    return {
-        'password_ok': password_ok,
-        'length_error': length_error,
-        'digit_error': digit_error,
-        'uppercase_error': uppercase_error,
-        'lowercase_error': lowercase_error,
-        'symbol_error': symbol_error,
-    }
