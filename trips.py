806bb2ae2fca9073192274f6d587276975d89bc2
Shumit Taher <shumitt@gmail.com>
2024-01-17 12:26:55 +0600

    created partcipants table

--- trips.py
+++ trips.py
@@ -1,4 +1,4 @@
-from flask import request, redirect, render_template, session
+from flask import request, redirect, render_template, session, flash
 from app import db
 
 
@@ -8,6 +8,11 @@ def create():
         tripname = request.form.get("tripname")
         tripdesc = request.form.get("tripdesc")
         db.execute("INSERT INTO trips (trip_title, trip_descrip, owner_id) VALUES (?,?,?)", tripname, tripdesc, session["user_id"])
+        flash("Trip Recorded")
         return redirect("/create")
 
     return render_template("create.html")
+
+def add_participents():
+
+    db.execute("INSERT INTO trips (trip_title, trip_descrip, owner_id) VALUES (?,?,?)", tripname, tripdesc, session["user_id"])
