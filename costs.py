d295274ff94126747ce487f7d145ef13e1508801
Shumit Taher <shumitt@gmail.com>
2024-02-04 11:37:12 +0600

    cost lines added

--- costs.py
+++ costs.py
@@ -1,6 +1,8 @@
 from flask import request, redirect, render_template, session, flash, url_for
 from trips_helpers import process_trip_id, owned
 from costs_helpers import add_costs
+from app import db
+
 
 def costs():
 
@@ -19,6 +21,9 @@ def costs():
     selected_trip_id = request.args.get("trip_id")
     selected_trip = process_trip_id(selected_trip_id)
 
+    cost_lines = db.execute('''SELECT * 
+                            FROM costs
+                            WHERE trip_id = ?
+                            ''', selected_trip_id)
 
-
-    return render_template("costs.html", selected_trip = selected_trip)
+    return render_template("costs.html", selected_trip = selected_trip, cost_lines = cost_lines)
