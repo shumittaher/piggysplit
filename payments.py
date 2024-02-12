75c415bf9b273a32f37fd7d745e939e91fdf2b84
Shumit Taher <shumitt@gmail.com>
2024-02-12 14:38:26 +0600

    payment routing ok

--- payments.py
+++ payments.py
@@ -1,7 +1,13 @@
-from flask import render_template, session
+from flask import render_template, session, request
 from trips_helpers import fetch_user_trips
 
 def payments():
+
+    if request.method == "POST":
+        participant_id = request.form.get("participant_id")
+        payment_amount = request.form.get("payment_amount")
+        trip_id = request.form.get("trip_id")
+        print(participant_id, payment_amount, trip_id)
 
     user_id = session.get("user_id")
     users_trips = fetch_user_trips(user_id)
