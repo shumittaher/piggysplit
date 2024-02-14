40e8afcbfe478455fa7a5b3fceba2d6e7b878a21
Shumit Taher <shumitt@gmail.com>
2024-02-14 13:23:17 +0600

    payment insert implemented

--- payments.py
+++ payments.py
@@ -1,14 +1,18 @@
 from flask import render_template, session, request
 from trips_helpers import fetch_user_trips
+from payments_helpers import record_payment
 
 def payments():
+    
+    user_id = session.get("user_id")
 
     if request.method == "POST":
         participant_id = request.form.get("participant_id")
         payment_amount = request.form.get("payment_amount")
+        payment_desc = request.form.get("payment_desc")
         trip_id = request.form.get("trip_id")
+        record_payment(user_id, participant_id, trip_id, payment_desc, payment_amount)
 
-    user_id = session.get("user_id")
     users_trips = fetch_user_trips(user_id)
 
     return render_template("payments.html", users_trips = users_trips)
--- payments_helpers.py
+++ payments_helpers.py
@@ -1,6 +1,7 @@
 from app import db
 
-def record_payment(payer_id, payee_id, trip_id):
+def record_payment(payer_id, payee_id, trip_id, payment_desc, payment_amount):
     db.execute('''INSERT INTO payments
-               (,,trip_id)
-               VALUES (?, ?, ?) ''', payer_id, payee_id, trip_id)
+               (payer_id, paid_to, trip_id, payment_description, payment_amount)
+               VALUES (?, ?, ?, ?, ?) ''', 
+               payer_id, payee_id, trip_id, payment_desc, payment_amount)
