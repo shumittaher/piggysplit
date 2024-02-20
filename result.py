ae33c879723b639bebbaebb703a7a5e459d07ca7
Shumit Taher <shumitt@gmail.com>
2024-02-20 14:50:57 +0600

    resultsb fix

--- result.py
+++ result.py
@@ -2,7 +2,7 @@ from flask import Flask, flash, redirect, render_template, request, jsonify, ses
 from costs_helpers import get_common_cost, get_induvidual_cost, total_trip_cost
 from trips_helpers import get_participants, fetch_user_trips, process_trip_id
 from payments_helpers import total_recevied, total_paid
-from result_helpers import format_fixer
+from result_helpers import format_fixer, object_to_row, outstandings_row
 
 
 
@@ -30,26 +30,6 @@ def result():
         outstandings.append(participant_row)
 
     return render_template("result.html", outstandings = outstandings, vendor_row = vendor_row, trip_details = trip_details)
-
-class outstandings_row:
-    def __init__(self, party, payable_amounts, received_amount, paid_amounts):
-        self.party= party
-        self.received_amount = received_amount
-        self.payable_amounts = payable_amounts
-        self.paid_amounts = paid_amounts
-
-    def calculate_outstanding(self):
-        return (self.payable_amounts - self.paid_amounts + self.received_amount)
-    
-
-def object_to_row(obj):
-    return {
-                "party" : obj.party, 
-                "received_amount": format_fixer(obj.received_amount), 
-                "payable_amounts":format_fixer(obj.payable_amounts), 
-                "paid_amounts": format_fixer(obj.paid_amounts), 
-                "outstanding_amount":format_fixer(obj.calculate_outstanding())
-            }
 
 def results_selection():
     
--- result_helpers.py
+++ result_helpers.py
@@ -16,4 +16,22 @@ def format_fix_table(table, fix_key):
         lines.append(newline)
 
     return lines
+
+def object_to_row(obj):
+    return {
+                "party" : obj.party, 
+                "received_amount": format_fixer(obj.received_amount), 
+                "payable_amounts":format_fixer(obj.payable_amounts), 
+                "paid_amounts": format_fixer(obj.paid_amounts), 
+                "outstanding_amount":format_fixer(obj.calculate_outstanding())
+            }
  
+class outstandings_row:
+    def __init__(self, party, payable_amounts, received_amount, paid_amounts):
+        self.party= party
+        self.received_amount = received_amount
+        self.payable_amounts = payable_amounts
+        self.paid_amounts = paid_amounts
+
+    def calculate_outstanding(self):
+        return (self.payable_amounts - self.paid_amounts + self.received_amount)
