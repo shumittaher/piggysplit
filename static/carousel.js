ad0f71be6e068eeccece3e259d0f4fccfdd598d3
Shumit Taher <shumitt@gmail.com>
2024-03-07 16:01:53 +0600

    carousal fix caption

--- static/carousel.js
+++ static/carousel.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    trip_id_area = document.getElementById("trip_id_area")
-    trip_desc_area = document.getElementById("trip_desc_area")
-    payment_form_area = document.getElementById("payment_form_area")
-    participant_options = document.getElementById("participant_options")
-    payment_details_table = document.getElementById("payment_details_table")
-    payment_details_area = document.getElementById("payment_details_area")
+    carousel = document.getElementById("car")
 })
